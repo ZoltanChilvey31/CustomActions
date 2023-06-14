5d025829572cea0c7d2d272b03f8d6462ceba833
Romain LEPOITTEVIN-LESVALLEES <rlepoittevinlesvallees@ige-xao.com>
2023-06-14 14:28:08 +0200

    js actions

---
+++ .github/actions/deploy-s3-js/main.js
@@ -0,0 +1,15 @@
+﻿const core = require('actions/core');
+const github = require('actions/github');
+const exec = require('actions/exec');
+function run(){
+    //1) get some input values
+    const bucket = core.getInput('bucket', {required: true});
+    const bucketRegion = core.getInput('bucket-region', {required: true});
+    const dist = core.getInput('dist-folder', {required: true});
+    
+    //2) upload files
+    const s3Uri= `s3://${bucket}`;
+    exec.exec('aws s3 sync ${dist} ${s3Uri}');
+    core.notice('Hello from custom JS Action');
+}
+run();
